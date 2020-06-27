2b48325da6fbd2c2ea7fdc4b06a201101c2a5a53
arnaudjopart <arnaudjopart@users.noreply.github.com>
2020-06-27 18:39:18 +0200

    raycast dots

--- Assets/LineOfSight.cs
+++ Assets/LineOfSight.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
 using UnityEngine;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using RaycastHit = Unity.Physics.RaycastHit;
 
 public class LineOfSight : MonoBehaviour
 {
@@ -17,24 +22,33 @@ public class LineOfSight : MonoBehaviour
     private LayerMask m_layerMask;
 
     private Vector3[] m_raycastHiPositions;
+
+    //private NativeArray<RaycastHit> m_results;
+    private EntityManager m_entityManager;
+    //private NativeArray<RaycastHit> m_result;
+
     // Start is called before the first frame update
     void Start()
     {
-        m_raycastHiPositions = new Vector3[m_numberOfRaycast];
+        m_entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        m_raycastHiPositions = new Vector3[m_numberOfRaycast]; 
         
     }
 
     private void DrawMesh()
     {
+        var collisionWorld = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<BuildPhysicsWorld>()
+        .PhysicsWorld.CollisionWorld;
+        
         var step = m_amplitudeOfSightInDegrees / (m_numberOfRaycast-1);
         m_raycastHiPositions = new Vector3[m_numberOfRaycast];
         double startTimer = Time.realtimeSinceStartup;
+        
         for (var i = 0; i < m_numberOfRaycast; i++)
         {
             var rayCastDirection = Quaternion.Euler(0, (-m_amplitudeOfSightInDegrees*.5f)+(i*step), 0)*transform.forward;
-            var ray = new Ray(transform.position,rayCastDirection);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, m_maxDistance, m_layerMask))
+            var ray = new UnityEngine.Ray(transform.position,rayCastDirection);
+            if (Physics.Raycast(ray, out var hit, m_maxDistance, m_layerMask))
             {
                 m_raycastHiPositions[i] = hit.point;
             }
@@ -43,7 +57,63 @@ public class LineOfSight : MonoBehaviour
                 m_raycastHiPositions[i] = ray.GetPoint(m_maxDistance);
             }
         }
+        
+        /*
+        for (var i = 0; i < m_numberOfRaycast; i++)
+        {
+            var firstRaycastHit = m_raycastHiPositions[i];
+            float distance;
+            distance = Vector3.Distance(transform.position,firstRaycastHit);
+            var rayCastDirection = Quaternion.Euler(0, (-m_amplitudeOfSightInDegrees*.5f)+(i*step), 0)*transform.forward;
+            Debug.DrawRay(transform.position,rayCastDirection*distance);
+        }
+        */
+        /*
+        var inputs = new NativeArray<RaycastInput>(m_numberOfRaycast,Allocator.TempJob);
+        var result = new NativeArray<RaycastHit>(m_numberOfRaycast,Allocator.TempJob);
+        
+        for (var i = 0; i < m_numberOfRaycast; i++)
+        {
+            var rayCastDirection = Quaternion.Euler(0, (-m_amplitudeOfSightInDegrees*.5f)+(i*step), 0)*transform.forward;
+            var raycastInput = new RaycastInput
+            {
+                Start = transform.position,
+                End = transform.position+rayCastDirection*m_maxDistance,
+                Filter = new CollisionFilter
+                {
+                    BelongsTo = ~0u,
+                    CollidesWith = ~0u, // all 1s, so all layers, collide with everything
+                    GroupIndex = 0
+                }
+            };
 
+            inputs[i] = raycastInput;
+
+        }
+
+        
+        DotsRaycastManager.MultipleRaycast(collisionWorld,inputs,ref result);
+        /*
+        
+        for (var i = 0; i < m_numberOfRaycast; i++)
+        {
+            var firstRaycastHit = result[i];
+            float distance;
+            if (m_entityManager.Exists(firstRaycastHit.Entity))
+            {
+                distance = Vector3.Distance(transform.position,firstRaycastHit.Position);
+            }
+            else
+            {
+                distance = m_maxDistance; 
+            }
+            var rayCastDirection = Quaternion.Euler(0, (-m_amplitudeOfSightInDegrees*.5f)+(i*step), 0)*transform.forward;
+            Debug.DrawRay(transform.position,rayCastDirection*distance);
+        }
+        inputs.Dispose();
+        result.Dispose();
+        
+        /*
         Mesh mesh = new Mesh();
         var nbOfTriangles = m_numberOfRaycast - 1;
         var nbOfVertices = nbOfTriangles * 3;
@@ -75,7 +145,7 @@ public class LineOfSight : MonoBehaviour
         /*var normals = new Vector3[3];
         uvs[0] = new Vector3(0,1);
         uvs[1] = new Vector3(0,1);
-        uvs[2] = new Vector3 (0,1);*/
+        uvs[2] = new Vector3 (0,1);
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
@@ -83,26 +153,26 @@ public class LineOfSight : MonoBehaviour
         
         //mesh.normals = normals;
         m_meshFilter.mesh = mesh;
-        
+        */
         var endTimer = Time.realtimeSinceStartup;
         var workTime = (endTimer - startTimer)*1000;
-        //Debug.Log(workTime+" ms");
+        Debug.Log(workTime+" ms");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (!Input.GetMouseButtonDown(0)) return;
         DrawMesh();
     }
 
     private void OnDrawGizmos()
     {
-        var step = m_amplitudeOfSightInDegrees / (m_numberOfRaycast-1);
-        for (var i = 0; i < m_numberOfRaycast; i++)
-        {
-            var distance = Vector3.Distance(transform.position,m_raycastHiPositions[i]);
-            var rayCastDirection = Quaternion.Euler(0, (-m_amplitudeOfSightInDegrees*.5f)+(i*step), 0)*transform.forward;
-            Debug.DrawRay(transform.position,rayCastDirection*distance);
-        }
+        
+    }
+
+    private void OnDestroy()
+    {
+        
     }
 }
---
+++ Assets/Tutorial/Scripts/DotsRaycastManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Physics;
+
+
+public class DotsRaycastManager
+{
+    [BurstCompile]
+    public struct RaycastJob : IJobParallelFor
+    {
+        [ReadOnly] public CollisionWorld m_world;
+        [ReadOnly] public NativeArray<RaycastInput> m_inputs;
+        public NativeArray<RaycastHit> m_results;
+        
+        public void Execute(int index)
+        {
+            var hit = new RaycastHit();
+            m_world.CastRay(m_inputs[index], out hit);
+            m_results[index] = hit;
+        }
+    }
+
+
+    public static JobHandle ScheduleBatchRaycast(CollisionWorld _collisionWorld, NativeArray<RaycastInput> _inputs,
+        NativeArray<RaycastHit> _results)
+    {
+        var raycastJob = new RaycastJob()
+        {
+            m_world = _collisionWorld,
+            m_inputs = _inputs,
+            m_results = _results
+        };
+
+        JobHandle jobHandle = raycastJob.Schedule(_inputs.Length, 4);
+        return jobHandle;
+    }
+
+    public static void SingleRaycast(CollisionWorld _collisionWorld, RaycastInput _input,
+        ref RaycastHit _result)
+    {
+        var singleInput = new NativeArray<RaycastInput>(1,Allocator.TempJob);
+        var singleResult = new NativeArray<RaycastHit>(1,Allocator.TempJob);
+
+        singleInput[0] = _input;
+
+        var handle = ScheduleBatchRaycast(_collisionWorld, singleInput, singleResult);
+        handle.Complete();
+        _result = singleResult[0];
+
+        singleInput.Dispose();
+        singleResult.Dispose();
+    }
+
+    public static void MultipleRaycast(CollisionWorld _collisionWorld, NativeArray<RaycastInput> _inputs,
+        ref NativeArray<RaycastHit> _results)
+    {
+        var handle = ScheduleBatchRaycast(_collisionWorld, _inputs, _results);
+        handle.Complete();
+    }
+    
+}
